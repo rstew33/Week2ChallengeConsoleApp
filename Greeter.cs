e24293094081bcf6d03b304ecf9ef8bc2157fcd9
Andrew Steward <rstew33@gmail.com>
2020-11-24 19:48:58 -0500

    initial commit

---
+++ Greeter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2ChallengeConsoleApp
+{
+    public class Greeter
+    {
+        public void GetName(string name)
+        {
+            string nameVar = Console.ReadLine();
+        }
+        public void Hello (string nameVar)
+        {
+            Console.WriteLine("Hello " + nameVar);
+        }
+
+        public void TimeofDay(string nameVar)
+        {
+            if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("Good Morning " + nameVar);
+            }
+            else if (DateTime.Now.Hour >=12 && DateTime.Now.Hour < 18)
+            {
+                Console.WriteLine("Good Afternoon " + nameVar);
+            }
+            else if (DateTime.Now.Hour >= 16 && DateTime.Now.Hour < 20)
+            {
+                Console.WriteLine("Good Evening " + nameVar);
+            }
+            else if (DateTime.Now.Hour >= 20)
+            {
+                Console.WriteLine("Good Night " + nameVar);
+            }
+        }
+        public void Goodbye (string nameVar)
+        {
+            Console.WriteLine("Goodbye " + nameVar);
+        }
+    }
+}
---
+++ Week2ChallengeConsoleApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week2ChallengeConsoleApp;
+
+namespace Week2ChallengeConsoleApp
+{
+    public class Week2ChallengeConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            var instance = new Greeter();
+            string userInput = Console.ReadLine();
+            instance.Hello(userInput);
+            instance.TimeofDay(userInput);
+            instance.Goodbye(userInput);
+            Console.WriteLine("Enter any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
